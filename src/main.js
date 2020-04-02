0c5c004f00a94ba8bda6b054b1172f5ca5b3eba0
joker-hkai <1270112884@qq.com>
2020-04-02 19:39:21 +0800

    heimatt init

--- src/main.js
+++ src/main.js
@@ -2,7 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 导入字体库
+import '@/style/font.css'
+import '@/style/base.css'
 
+// 导入组件包
+import '@/plugins/vant'
 Vue.config.productionTip = false
 
 new Vue({
---
+++ src/views/login/login.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="login">
+    <van-nav-bar title="登录" />
+    <van-field
+      v-model="loginData.mobile"
+      type="tel"
+      placeholder="请输入手机号"
+    >
+      <template slot="left-icon">
+        <span class="iconfont icon-iphone"></span>
+      </template>
+    </van-field>
+    <van-field
+      center
+      clearable
+      v-model="loginData.code"
+      placeholder="请输入验证码"
+    >
+      <template slot="left-icon">
+        <span class="iconfont icon-lock"></span>
+      </template>
+      <van-button round slot="button" size="small">发送验证码</van-button>
+    </van-field>
+    <van-button
+      type="info"
+      loading-type="spinner"
+      :loading="isLoading"
+      loading-text="登录中...."
+      @click="doLogin"
+      >登录</van-button
+    >
+  </div>
+</template>
+
+<script>
+import { login } from '@/api/login'
+export default {
+  data() {
+    return {
+      loginData: {
+        mobile: '13476894603',
+        code: '246810'
+      },
+      isLoading: false
+    }
+  },
+  methods: {
+    async doLogin() {
+      const { data } = await login(this.loginData)
+      console.log(data)
+    }
+  }
+}
+</script>
+
+<style lang="less">
+.login {
+  height: 100%;
+  background-color: #f5f7f9;
+  .van-nav-bar.van-hairline--bottom {
+    background-color: #3e9df8;
+
+    .van-nav-bar__title.van-ellipsis {
+      color: white;
+    }
+  }
+
+  button.van-button.van-button--default.van-button--small {
+    background-color: #ededed;
+  }
+
+  button.van-button.van-button--info.van-button--normal {
+    width: 90%;
+    margin: 30px auto;
+    display: block;
+  }
+}
+</style>
